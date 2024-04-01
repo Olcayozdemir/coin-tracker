4054d96441f30c9829c999fe28760f34b084bd1d
Olcay Özdemir <31245787+Olcayozdemir@users.noreply.github.com>
2024-04-01 13:42:28 +0300

    Update general.ts

--- src/configs/general.ts
+++ src/configs/general.ts
@@ -1,7 +1,5 @@
 import { QueryClient } from "react-query";
 
-export const STALE_TIME = 1000 * 10;
-
 export const queryClient = new QueryClient({
-  defaultOptions: { queries: { staleTime: STALE_TIME, retry: 3 } },
+  defaultOptions: { queries: { retry: 3 } },
 });
