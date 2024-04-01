6879e5997fe3dc0da2f6009d55b103b73d05ffed
Olcay Özdemir <31245787+Olcayozdemir@users.noreply.github.com>
2024-04-01 13:33:15 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import CoinListing from "@/containers/CoinListing";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-export const STALE_TIME = 1000 * 10;
+const STALE_TIME = 1000 * 10;
 
 export const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: STALE_TIME, retry: 3 } },
